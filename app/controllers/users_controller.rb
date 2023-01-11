606c1218a88dc2791f12dc4a1383a76357ecb9cb
carolinedeppe <rcdeppe@gmail.com>
2023-01-11 16:59:16 +0000

    Build the authentication system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+  skip_authentication only: [:new, :create]
   def new
     @user = User.new 
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -17,6 +19,7 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.require(:user).permit(:name, :email, :password) 
   end
